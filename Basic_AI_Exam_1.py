f1f74902dc906e429b3549f678e298aeaf5b6a84
JEONJinah <gorhfekwls@naver.com>
2021-10-19 10:46:15 +0900

    UPLOAD

---
+++ Basic_AI_Exam_1.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:52d2f72cab3ad80f9f7e96d329dd1f817641537d3528e52a1d87ea619e4423f7
+size 1070
---
+++ bubble_sort.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0af7a39e66286cb4ee62bf43a7934ed999b7e6d74d52a4e8b673a27fa39b2504
+size 6831
